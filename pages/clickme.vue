a35376740a2f2550ac956270cbe03a24a1cb513e
Shivani Bhardwaj <shivanib134@gmail.com>
2021-09-24 14:27:17 +0530

    fix links on cards

--- pages/clickme.vue
+++ pages/clickme.vue
@@ -5,22 +5,22 @@
         <p class="px-12 text-left text-2xl">Below is the list of a few open source projects that are awesome and I endorse.</p>
         <p class="px-12 text-left text-2xl">If you also use any of these projects, please consider donating. If you are not in a position to donate, please check their GitHub and leave a &#11088;!</p>
         <div class="flex-col p-10">
-            <div class="card text-center">
-                <a href="https://oisf.net/memberships"></a>
+            <div class="myBox card text-center" onclick="location.href='https://oisf.net/memberships'">
+                <a href="#"></a>
                 <div>
                     <h2 class="font-bold">Suricata</h2>
                 </div>
             </div>
             </br>
-            <div class="card text-center">
-                <a href="https://www.rust-lang.org/sponsors"></a>
+            <div class="card text-center" onclick="location.href='https://www.rust-lang.org/sponsors';">
+                <a href="#"></a>
                 <div>
                     <h2 class="font-bold">Rust</h2>
                 </div>
             </div>
             </br>
-            <div class="card text-center">
-                <a href="https://opencollective.com/nuxtjs#contribute"></a>
+            <div class="card text-center" onclick="location.href='https://opencollective.com/nuxtjs#contribute'">
+                <a href="#"></a>
                 <div>
                     <h2 class="font-bold">Nuxt.js</h2>
                 </div>
