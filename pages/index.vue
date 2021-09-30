219297fa6bdc22b735f4c096f0b6ccf3afb6b2cf
Shivani Bhardwaj <shivanib134@gmail.com>
2021-10-01 00:51:12 +0530

    update pictures

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,26 @@
   <div>
         <h1 class="pt-12 pb-8 text-center text-5xl font-extrabold">Hello, beautiful human!</h1>
         <p>Welcome to my little corner on the web.</p>
-        <p>Here are some unedited pretty pictures to lift your mood and... fill this page up. &#128517;</p>
+        <p>Here are some pretty pictures to lift your mood and... fill this page up. &#128517;</p>
         <br />
         <div class="grid grid-cols-3 gap-2 items-center">
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/lake.JPG' alt="Mirror Lake">
+            <img src='~assets/lake.jpg' alt="Mirror Lake">
           </div>
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/lone-red-tree.JPG' alt="Lone Red Tree">
+            <img src='~assets/lone-red-tree.jpg' alt="Lone Red Tree">
           </div>
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/rain-mountains.JPG' alt="Pouring on Mountains">
+            <img src='~assets/rain-mountains.jpg' alt="Pouring on Mountains">
           </div>
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/jardin-luxem.JPG' alt="Grey in Sky Rainbow on Earth">
+            <img src='~assets/jardin-luxem.jpg' alt="Grey in Sky Rainbow on Earth">
           </div>
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/paris-eiffel.JPG' alt="Symbol of Love">
+            <img src='~assets/paris-eiffel.jpg' alt="Symbol of Love">
           </div>
           <div class="m-auto p-3 border-2 border-grey w-4/5">
-            <img src='~assets/tree-chandelier.JPG' alt="Tree Chandelier">
+            <img src='~assets/tree-chandelier.jpg' alt="Tree Chandelier">
           </div>
         </div>
   </div>
