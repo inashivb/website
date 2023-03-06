11549d67b43f3b1f46861f9acd9150d27558eef2
Guinnes Singh <gswebspace@gmail.com>
2023-03-07 00:11:30 +0530

    Fixes footer overlap with content

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
-        <Nuxt class="md:h-screen"/>
+        <Nuxt class=""/>
         <br /><br /><br />
         <Footer />
     </div>
